585f7c792c983826d87ca0776ecd29cad7317b8e
Serap <77828715+serapo@users.noreply.github.com>
2022-06-06 12:03:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const tahmin=document.getElementById("tahmin");
+const  btn=document.getElementById("btn");
+const sonuc=document.getElementById("sonuc");
+const between=document.getElementById("between");
+
+
+
+let random=Math.floor(Math.random()*101);
+let hak=5;
+
+console.log(random);
+btn.addEventListener('click',(e)=>{
+    if(!tahmin.value){
+             alert("Lütfen 0-100 arası sayı giriniz: ");
+    }
+    else if(tahmin.value>100){
+        alert("Girdiğiniz sayı 100 den büyük olmamalı ");
+    }
+      
+       else if(tahmin.value.toString()===random.toString()){
+            // sonuc.innerHTML=`<p>Tebrikler tahmin ettiniz</p>`;
+            // tahmin.value="";
+            // hak=0;
+            ysYonlendir();
+        
+        }   
+        else if(random>=50){        
+            if(tahmin.value<random){
+            //  alert("Tahmin edilen sayı , rastgele sayıdan küçüktür")
+            //   sonuc.innerHTML=`<p>${tahmin.value} - 100 arasında bir sayı giriniz</p>`;
+            sonuc.innerHTML=`<p>${tahmin.value}-100 arasında bir sayı giriniz</p>`;
+            tahmin.value="";
+            }
+            else if(tahmin.value>random){
+                sonuc.innerHTML=`<p> 50- ${tahmin.value} arasında bir sayı giriniz</p>`;
+                tahmin.value="";
+            }
+        }    
+        else if(random<50){
+           
+            if(tahmin.value>random){
+                // alert("Tahmin edilen sayı , rastgele sayıdan küçüktür")
+                 sonuc.innerHTML=`<p>${tahmin.value} - ${tahmin.value} arasında bir sayı giriniz</p>`;
+                tahmin.value="";
+ 
+             }
+             else if(tahmin.value<random){
+                 sonuc.innerHTML=`<p> ${tahmin.value} - 50 arasında bir sayı giriniz</p>`;
+                 tahmin.value="";
+             }
+        }
+        
+               //sonuc.innerHTML=`<p>${tahmin.value}</p>`;
+      
+            
+  
+        
+        // else if(random<50){
+        //     sonuc.innerHTML=`<h2>${tahmin.value}</p>`;
+        // }
+      
+    });
+
+    function ysYonlendir(){
+      window.location.href='winner.html';
+    }
+    
